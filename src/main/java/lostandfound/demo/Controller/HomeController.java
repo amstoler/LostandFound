a87609bf56967681dc38f820b0ba7ad6544db256
Ariel Stoler <amstoler@gmail.com>
2018-03-05 15:09:15 -0500

    fixed bug so that admin can reliably toggle and set lost and found status of item.

--- src/main/java/lostandfound/demo/Controller/HomeController.java
+++ src/main/java/lostandfound/demo/Controller/HomeController.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
-//@RequestMapping()
 public class HomeController {
     @Autowired
     AppRoleRepository appRoleRepository;
@@ -30,16 +29,11 @@ public class HomeController {
     @GetMapping(value = {"/", "/home"})
     public  String showHome() {return "index";}
 
-    @GetMapping("/index")
-    public  String index() {return "index";}
-
     @PostMapping("/login")
     public String login()
     {
         return "/login2";
     }
-
-
 
     @GetMapping("/register")
     public String registerUser(Model model)
@@ -62,51 +56,6 @@ public class HomeController {
         return "redirect:/";
     }
 
-
-//Tested and Works! Commentating out to improve.
-   /* @GetMapping("/displayItems")
-    public String showdisplayItems(Model model) {
-        model.addAttribute("item", new Item());
-        model.addAttribute("item", itemRepo.findAll());
-
-        return "displayItems2";
-    }*/
-
-    //Testing below to filter for item status.
-    @GetMapping("/displayItems")
-    public String showdisplayItems(Model model) {
-        model.addAttribute("item", new Item());
-        model.addAttribute("item", itemRepo.findAllByitemStatusContainingIgnoreCase("lost"));
-
-        //Below displays all items weather lost or found
-       // model.addAttribute("item", itemRepo.findAll());
-      /*  if(item.getItemStatus().equalsIgnoreCase("Lost")){
-            model.addAttribute("item",appUserRepository.findAll());
-
-        }
-
-     if((itemRepo.findAllByitemStatusContainingIgnoreCase(String "lost") {
-            model.addAttribute("item", appUserRepository.findAll());
-        }*/
-
-        return "displayItems2";
-    }
-
-
-// Needs to be tested more. This should be list of ONLY lost items
-/*@GetMapping("/lostitmes")
-public String lostitemslist(@ModelAttribute("item") Item item,Model model){
-
-    if(item.getItemStatus().equalsIgnoreCase("Lost")){
-        model.addAttribute("item",appUserRepository.findAll());
-
-    }
-
-    return "redirect:/displayItems2";
-
-
-}*/
-
       @GetMapping("/addItem")
     public String showItemForm(Model model) {
         model.addAttribute("item", new Item());
@@ -116,7 +65,7 @@ public String lostitemslist(@ModelAttribute("item") Item item,Model model){
     }
 
     @PostMapping("/processItem")
-    public String displayItems(@Valid @ModelAttribute("item") Item item, Model model, BindingResult result, Authentication auth) {
+    public String lostitems(@Valid @ModelAttribute("item") Item item, Model model, BindingResult result, Authentication auth) {
         if (result.hasErrors()) {
             return "itemForm2";
         }
@@ -134,27 +83,32 @@ public String lostitemslist(@ModelAttribute("item") Item item,Model model){
         //End of show. This method existed before to display ALL items but modified to save items
         // to currently logged in user.
 
-        //Allows EVERYTHING stored in ItemRepo to display to Itemform.
         model.addAttribute("item", itemRepo.findAll());
-//testing needed if want to display to return displayitems2
+//testing needed if want to display to return lostitems html page
         return "home";
 
     }
 
-    //incomplete method NEEDS TESTING
     @GetMapping("/founditems")
     public String showfounditems(Model model, Authentication auth) {
         model.addAttribute("item", new Item());
         AppUser appUser = appUserRepository.findAppUserByUsername(auth.getName());
         model.addAttribute("item", itemRepo.findAllByitemStatusContainingIgnoreCase("found"));
-        //below line added to test and filter for only FOUND items
-
-        //model.addAttribute("item", itemRepo.findAllByitemStatusContainingIgnoreCase("found"));
-       // model.addAttribute("founditems", appUser.getItems(itemRepo.findAllByitemStatusContainingIgnoreCase("found")));
 
         return "founditems";
 
     }
+    // Testing below method to look same as above but for lost
+    @GetMapping("/lostitems")
+    public String showlostitems(Model model, Authentication auth) {
+        model.addAttribute("item", new Item());
+        AppUser appUser = appUserRepository.findAppUserByUsername(auth.getName());
+        model.addAttribute("item", itemRepo.findAllByitemStatusContainingIgnoreCase("lost"));
+
+        return "lostitems";
+
+    }
+
 
 
     @GetMapping("/additemtofound/{id}")
@@ -169,7 +123,7 @@ public String lostitemslist(@ModelAttribute("item") Item item,Model model){
         appUserRepository.save(appUser);
         model.addAttribute("listusers", appUserRepository.findAll());
         model.addAttribute("itemlist", appUserRepository.findAll());
-        return "redirect:/displayItems";
+        return "redirect:/lostitems";
     }
 
     //Insert @GetMapping("/additemtolost/{id}")
@@ -178,16 +132,15 @@ public String lostitemslist(@ModelAttribute("item") Item item,Model model){
     public String additemtolostlist(@PathVariable("id") long id, Model model, Authentication auth){
 
         Item item = itemRepo.findOne(id);
-//        Must use database user not spring security user
+
         AppUser appUser = appUserRepository.findAppUserByUsername(auth.getName());
         appUser.addItemtoAppUser(item);
         item.setItemStatus("Lost");
         model.addAttribute("lostitemslist", itemRepo.findOne(id));
         itemRepo.save(item);
         appUserRepository.save(appUser);
-        model.addAttribute("userlist",appUserRepository.findAll());
-        model.addAttribute("itemslist",appUserRepository.findAll());
-        return "redirect:/displayItems";
+        model.addAttribute("item",appUserRepository.findAll());
+        return "redirect:/founditems";
     }
 
 
--- src/main/java/lostandfound/demo/Repositories/ItemRepo.java
+++ src/main/java/lostandfound/demo/Repositories/ItemRepo.java
@@ -5,9 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepo extends CrudRepository<Item, Long> {
     Item findItemByTitle(String roleName);
-    //Iterable<Item> findAllByItemNameContainingIgnoreCase(String searchitems);
-
-    //Iterable<Item> findAllByitemStatusContainingIgnoreCase(String itemStatus);
-
     Iterable<Item> findAllByitemStatusContainingIgnoreCase(String itemStatus);
 }
